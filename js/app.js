536c5f68b4d0b0f65dcfe7f61e35bbf13fb03717
Alizadekh <aykhanalizadekh@gmail.com>
2024-02-25 00:41:22 +0400

    Fixed some bugs

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const menu = document.querySelector(".mobile-menu");
 
 burger.addEventListener("click", function () {
   if (menu.style.transform === "translateX(300px)") {
-    menu.style.transform = "translateX(-50px)";
+    menu.style.transform = "translateX(-45px)";
   } else {
     menu.style.transform = "translateX(300px)";
   }
